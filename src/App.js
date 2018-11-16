7d66570b208e4c721a53a75c0be0049ada008e50
salvescoding <salves2383@gmail.com>
2018-11-16 12:02:45 +0100

    Implement css changes on components

--- src/App.js
+++ src/App.js
@@ -13,8 +13,17 @@ class App extends Component {
   switchNameHanlder = (newName) => {
     this.setState({
       persons: [
-        { name: "Salves", age: 35},
-        { name: "Maria", age: 19}
+        { name: newName, age: 35},
+        { name: 'Maria', age: 19}
+      ]
+    })
+  }
+
+  nameChangeHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Salves', age: 35},
+        { name: event.target.value , age: 26}
       ]
     })
   }
@@ -24,14 +33,17 @@ class App extends Component {
       <div className="App">
         <h1>This is my first React App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHanlder}>Switch names</button>
+        <button 
+        className='Home-Button'
+        onClick={() => this.switchNameHanlder('Salves')}>Switch names</button>
         <Person
          name={this.state.persons[0].name}
          age={this.state.persons[0].age}
-         click={this.switchNameHanlder} />
+         click={this.switchNameHanlder.bind(this, 'Andre!')} />
         <Person
          name={this.state.persons[1].name} 
-         age={this.state.persons[1].age}>My hobbie is Yoga! </Person>
+         age={this.state.persons[1].age}
+         change={this.nameChangeHandler}  >My hobbie is Yoga! </Person>
       </div>
     );
   }
