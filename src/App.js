168832ac4d43b7f587369e85cc072e17358f8088
salvescoding <salves2383@gmail.com>
2018-11-23 17:40:09 +0100

    Add inline styles to the App. Install Radium package for media queries and other css styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium, {StyleRoot} from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -37,6 +38,20 @@ class App extends Component {
   }
 
   render() {
+
+    const style = {
+      backgroundColor: 'green',
+      color: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
+    };
+
     let persons = null;
 
     if(this.state.showPersons) {
@@ -53,20 +68,36 @@ class App extends Component {
           })}
         </div>
       )
+      style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      };
     }
 
+    let classes = [];
+    if(this.state.persons.length < 2) {
+      classes.push('red')
+    }
+    if (this.state.persons.length < 1) {
+      classes.push('bold')
+    }
+
+    
 
     return (
-      <div className="App">
-        <h1>This is my first React App</h1>
-        <p>This is really working!</p>
-        <button 
-        className='Home-Button'
-        onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        { persons }
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <h1>This is my first React App</h1>
+          <p className={classes.join(" ")} >This is really working!</p>
+          <button 
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+          { persons }
+        </div>
+      </StyleRoot>
     );
   }
 }
 
-export default App;
+export default Radium(App);
